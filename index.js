c9fd201731dd29ae10fcd99a92a5e6b28514f34a
gloria oduor <gloriaoduor@gmail.com>
2022-06-14 09:31:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function addingEventListener() {
     const input = document.getElementById('input');
 
-    input.addEventListener('click', function() {alert('I was clicked!');});
+    input.addEventListener('click', clickAlert);
 }
+
+function clickAlert() {
+    alert('I was clicked!');
+}
+
 addingEventListener()
